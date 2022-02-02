0f5b0865375505c283c6478a2299cccd61117c49
Jun Wei <58636401+leejunweisg@users.noreply.github.com>
2022-02-03 00:26:02 +0800

    updated incorrect serial path

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@
 
 # STM32 BOARD SERIAL CONNECTION
 # SERIAL_PORT = "/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0"
-SERIAL_PORT = "/dev/serial/ttyUSB0"
+SERIAL_PORT = "/dev/ttyUSB0"
 BAUD_RATE = 115200
