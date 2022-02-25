59c1ca799fd422d5ca1d6cb455f4d063e9ddf08d
Jun Wei <58636401+leejunweisg@users.noreply.github.com>
2022-02-25 09:03:23 +0800

    updated settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,10 @@
 # contains various configuration settings for the project
 
 # STM32 BOARD SERIAL CONNECTION
-# SERIAL_PORT = "/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0"
-SERIAL_PORT = "/dev/ttyUSB0"
+# SERIAL_PORT = "/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0"  # nodemcu esp8266 testing board
+SERIAL_PORT = "/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0002-if00-port0"  # stm32
 BAUD_RATE = 115200
 
 # API DETAILS
-API_IP = '192.168.2.10'
+API_IP = '192.168.28.15'  # tran's laptop hosting the api
 API_PORT = 5000
-
